c35c7cae5411b4a5f1d08888f9f7098004b724b3
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-07-08 12:38:01 +0000

    new commit

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Returning a Task"""
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     function that takes an integer max_delay and returns a asyncio.Task
     """
