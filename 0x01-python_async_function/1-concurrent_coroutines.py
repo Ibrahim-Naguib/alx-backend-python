aae492e663d4d09a4288ee86cb4d6577f658048f
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-07-08 12:33:56 +0000

    new commit

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -14,5 +14,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         list of delays
     """
-    delays = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completet(tasks)]
