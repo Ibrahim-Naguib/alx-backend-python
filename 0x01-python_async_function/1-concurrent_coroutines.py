9df414193df8455e6a51ddc19f61607537c046f7
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-07-08 12:35:13 +0000

    new commit

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-"""Multiple coroutines at the same time with async """
+"""Contains a method that spawns wait_random n times with a
+specified delay between each call."""
 import asyncio
 from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -15,4 +17,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         list of delays
     """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completet(tasks)]
+    return [await task for task in asyncio.as_completed(tasks)]
