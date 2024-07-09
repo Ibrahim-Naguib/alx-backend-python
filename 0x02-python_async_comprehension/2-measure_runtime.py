8f54a420916a9186bcfe3c87b049915f221e1c2e
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-07-09 09:56:11 +0000

    new commit

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,11 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """measure the total runtime and return it."""
     s = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    task = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*task)
     e = time.perf_counter()
     return e - s
